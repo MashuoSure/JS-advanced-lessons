9ef0a0a639f0379e29a9dc14c8b134ac05a9967d
ParaculesSure <shou_ma.sjz@foxmail.com>
2018-05-03 22:23:38 +0800

    LS17

---
+++ 课程练习/LS17/js/demo01.js
@@ -0,0 +1,88 @@
+var superObj = {
+    x:1,
+    y:2
+};
+var subObj_First = Object.create(superObj);
+var subObj_Second = Object.create(superObj);
+subObj_First.__proto__.x = 5;
+console.log(subObj_Second.x);
+//5
+
+function Person(name){
+    this.name = name;
+}
+Person.prototype.age = 22;
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(id){
+    this.id = id;
+}
+Student.prototype = new Person("Mike");
+var s1 = new Student(2017001);
+var s2 = new Student(2017002);
+
+console.log(s1.name,s1.age,s1.id);
+console.log(s2.name,s2.age,s2.id);
+s1.__proto__.name = "Jack";
+console.log(s2.name);
+s2.__proto__.__proto__.age = 99;
+console.log(s2.age);
+//Mike 22 2017001
+//Mike 22 2017002
+//Jack
+//99
+
+
+
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype.__proto__ = Person.prototype;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
+
+//{showName: ƒ, constructor: ƒ}
+
+
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){
+    console.log(this.name);
+};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
+
+//ƒ Student(name,age,id){
+//    Person.call(this,name,age);
+//    this.id = id;
+//}
+
+
+
+var BaseClass = function() {};
+BaseClass.prototype.f2 = function () {
+    console.log("This is a prototype method ");
+};
+BaseClass.f1 = function(){//定义静态方法
+    console.log("This is a static method ");
+};
+BaseClass.f1();
+var instance1 = new BaseClass();
+instance1.f2();
+
+//This is a static method 
+//This is a prototype method 
+
