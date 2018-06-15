02771a817f54a836b4d51f8baca0ae909c671fe9
ParaculesSure <shou_ma.sjz@foxmail.com>
2018-06-15 23:00:24 +0800

    LS28

--- 课程练习/LS01/js/demo01.js
+++
@@ -1,5 +0,0 @@
- var x = 10;
- document.onclick = function () {
- //console.log("x = ",x);
- alert("x = "+x);
-};
--- 课程练习/LS01/js/demo03.js
+++
@@ -1,6 +0,0 @@
-var a = 3;
-a+=5;
-console.log(a);
-console.log(typeof NaN);
-console.log(typeof Infinity);
-
